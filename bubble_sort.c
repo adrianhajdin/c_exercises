40755c81910db66895eea3b706c064fb966ca0f2
adrianhajdin <adrianhajdin00@gmail.com>
2020-03-20 09:35:15 +0100

    new exercise

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,11 +1,44 @@
-void bubble_sort(int array[], int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++){
-            if(array[j - 1] > array[j]) {
+#include <stdio.h>
+#include <stdint.h>
+ 
+int load_data(int polje[]){
+    int x = 0;
+    int input;
+ 
+    scanf("%d", &input);
+
+    while(input != 0) {
+        polje[x++] = input;
+
+        scanf("%d", &input);
+    }
+ 
+    return x;
+}
+ 
+void bubble_sort(int array[], int size){
+    for (int i = 0; i < size; i++) {
+        for (int j = 1; j < size; j++) {
+            if (array[j - 1] < array[j]) {
                 int tmp = array[j - 1];
                 array[j - 1] = array[j];
                 array[j] = tmp;
             }
-        }   
+        }
     }
+}
+ 
+int main() {
+    int polje[10000];
+    int n;
+ 
+    n = load_data(polje);
+
+    bubble_sort(polje, n);
+ 
+    for (int i = 0; i < n; i++){
+        printf("%d ", polje[i]);
+    }
+ 
+    return 0;
 }
